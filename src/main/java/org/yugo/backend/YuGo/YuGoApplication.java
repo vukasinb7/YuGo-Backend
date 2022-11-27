49a6dba34d9638f0c3d6333ba7d424953f871261
jokicjovan <jokicjovan@outlook.com>
2022-11-27 20:00:41 +0100

    Initial commit

---
+++ src/main/java/org/yugo/backend/YuGo/YuGoApplication.java
@@ -0,0 +1,13 @@
+package org.yugo.backend.YuGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yugo/backend/YuGo/YuGoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yugo.backend.YuGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
