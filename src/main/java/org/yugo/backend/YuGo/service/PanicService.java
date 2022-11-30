700cbde41e6da95654b13c23aac91b282e286089
jokicjovan <jokicjovan@outlook.com>
2022-11-30 15:15:19 +0100

    Added services

---
+++ src/main/java/org/yugo/backend/YuGo/service/PanicService.java
@@ -0,0 +1,14 @@
+package org.yugo.backend.YuGo.service;
+
+import org.yugo.backend.YuGo.model.Panic;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PanicService {
+    Panic add(Panic panic);
+
+    List<Panic> getAll();
+
+    Optional<Panic> get(Integer id);
+}
