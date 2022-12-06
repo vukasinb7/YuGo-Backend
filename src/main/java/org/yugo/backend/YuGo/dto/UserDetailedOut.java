9738caa35f2d439b29fd259d71d7d4caf32a4ca0
jokicjovan <jokicjovan@outlook.com>
2022-12-06 14:45:50 +0100

    Data.sql updated

--- src/main/java/org/yugo/backend/YuGo/dto/UserDetailedOut.java
+++ src/main/java/org/yugo/backend/YuGo/dto/UserDetailedOut.java
@@ -1,9 +1,11 @@
 package org.yugo.backend.YuGo.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.yugo.backend.YuGo.model.User;
 
+@NoArgsConstructor
 public class UserDetailedOut {
     @Getter
     @Setter
--- src/main/java/org/yugo/backend/YuGo/mapper/UserDetailedMapper.java
+++ src/main/java/org/yugo/backend/YuGo/mapper/UserDetailedMapper.java
@@ -1,5 +1,6 @@
 package org.yugo.backend.YuGo.mapper;
 
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
